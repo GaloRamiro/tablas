2a92b16e589ab84df1de28d65b2477c3e51e674b
GaloRamiro <g_cisneros_23@hotmail.es>
2026-04-23 20:29:22 -0500

    genera la tabla con el boton probar

--- multiplicar.js
+++ multiplicar.js
@@ -1,4 +1,9 @@
-function generarTablas(){
-    let contenedor = document.getElementById("miTabla");
-    contenedor.innerHTML="<h1>PROBANDO</h1>";
+function generarTablas() {
+  let contenido = "";
+  for (let i = 1; i <= 10; i++) {
+    let resulado = 3 * i;
+    contenido += `<div class="fila">3 x ${i} = ${resulado}</div>`;
+  }
+  let contenedor = document.getElementById("miTabla");
+  contenedor.innerHTML = contenido;
 }
