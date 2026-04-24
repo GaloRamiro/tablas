d025b12081bf3ccdef7131724453417a3665adf0
GaloRamiro <g_cisneros_23@hotmail.es>
2026-04-23 20:37:13 -0500

    ingreso numero y que imprima la variable

--- multiplicar.js
+++ multiplicar.js
@@ -1,8 +1,11 @@
 function generarTablas() {
+  let numero = document.getElementById("txtNumero");
+  let valorNumero = numero.value;
+  let enteroNumero = parseInt(valorNumero);
   let contenido = "";
   for (let i = 1; i <= 10; i++) {
-    let resulado = 3 * i;
-    contenido += `<div class="fila">3 x ${i} = ${resulado}</div>`;
+    let resulado = enteroNumero * i;
+    contenido += `<div class="fila">${enteroNumero} x ${i} = ${resulado}</div>`;
   }
   let contenedor = document.getElementById("miTabla");
   contenedor.innerHTML = contenido;
